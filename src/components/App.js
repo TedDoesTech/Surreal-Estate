df82adfa9776a1fe6f566ef4a3f8590e6cd6a816
Ed Crowther <edward930@hotmail.co.uk>
2023-04-18 17:20:25 +0100

    routing added and working basically

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import '../styles/app.css';
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  Link,
-} from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Navbar from './NavBar';
 import Properties from './Properties';
 import AddProperty from './AddProperty';
 
 const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <h2>Surreal Estate</h2>
-      </header>
-      <Navbar />
-      <Link className="item" to="/">
-        View Properties
-      </Link>
-      <Link className="item" to="/add-property">
-        Add Properties
-      </Link>
-      <Routes>
-        <Route path="/" element={Properties} />
-        <Route path="/add-property" element={AddProperty} />
-      </Routes>
-    </div>
-  </BrowserRouter>
+  <div className="App">
+    <header className="App-header">
+      <h2>Surreal Estate</h2>
+    </header>
+    <Navbar />
+    <Link className="item" to="/">
+      View Properties
+    </Link>
+    <Link className="item" to="/add-property">
+      Add Properties
+    </Link>
+    <Routes>
+      <Route path="/" element={Properties} />
+      <Route path="/add-property" element={AddProperty} />
+    </Routes>
+  </div>
 );
 
 export default App;
